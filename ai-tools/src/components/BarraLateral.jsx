15bd21ab550a4fa4df834124dc626c40ea39540e
Fernanda Díaz <110874443+cyberpeony@users.noreply.github.com>
2024-04-23 01:56:14 +0000

    Co-authored-by: Luis Omar <Luisssso2003@users.noreply.github.com> Co-authored-by: chrisAguil <chrisAguil@users.noreply.github.com>

---
+++ ai-tools/src/components/BarraLateral.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './BarraLateral.module.css';
+
+function BarraLateral() {
+    return (
+      <BarraLateral>
+        {/* HTML del footer aquí */}
+      </BarraLateral>
+    );
+  }
+  
+  export default BarraLateral;
--- ai-tools/src/components/BarraNavegacion.jsx
+++ ai-tools/src/components/BarraNavegacion.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Barra-navegacion.module.css';
+import styles from './BarraNavegacion.module.css';
 
 function BarraNavegacion() {
     return (
